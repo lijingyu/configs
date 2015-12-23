7fd2695ef67ff1403ef56bb63e672939fb325397
Jingyu Li <ljyat@sina.com>
2015-12-23 12:34:44 +0800

    add cssetl

--- setup.sh
+++ setup.sh
@@ -6,40 +6,45 @@ function creat_cscope()
         rm -rf cscope.files
     fi
 
-    echo "create cscope.files in $@"
+    echo_msg "create cscope.files in $@"
     for arg in $@
     do
         find  ${arg}  -type f |sed  -e '/ /d' -e 's:^\./::' -n -e '/\(\.\(\([chmSs]\)\|\(java\)\|\(cpp\)\|\(hp\)\|\(aidl\)\|\(rc\)\|\(cc\)\|\(def\)\|\(xml\)\|\(mk\)\)\|\([Mm]akefile\)\|\(Kconfig\)\)$/p' >> cscope.files
     done
 
-    echo "create cscope"
+    echo_msg "create cscope"
     cscope -bki cscope.files
 }
 
 function create_tags()
 {
-    echo "create tags"
+    echo_msg "create tags"
     if [ -s "cscope.files" ]; then
         ctags --c-kinds=-m --c++-kinds=-m --fields=+iaS --extra=+q  -L cscope.files
     else
-        echo "need cscope.files"
+        echo_msg "need cscope.files"
     fi
 }
 
 function create_dict()
 {
-    echo "create dict"
+    echo_msg "create dict"
     if [ -s "tags" ]; then
         sed 's/\s.*$//g' tags | uniq >  dict
     else
-        echo "need tags"
+        echo_msg "need tags"
     fi
+}
+
+function echo_msg()
+{
+    echo $@
 }
 
 function csset()
 {
     if [ $# -eq 0 ];then
-        echo "need dirname"
+        echo_msg "need dirname"
         return
     fi
 
@@ -48,13 +53,31 @@ function csset()
 
     create_tags
     create_dict
-    echo "create  success"
+    echo_msg "create  success"
+}
+
+function cssetl()
+{
+    if [ $# -eq 0 ];then
+        echo_msg "need dirname"
+        return
+    fi
+
+    cur_dir=`pwd`
+    for arg in $@
+    do
+        echo_msg "change dir: $arg"
+        cd $arg
+        csset .
+        echo_msg "change dir back"
+        cd $cur_dir
+    done
 }
 
 function csseta()
 {
     if [ $# -eq 0 ];then
-        echo "need dirname"
+        echo_msg "need dirname"
         return
     fi
 
@@ -71,19 +94,19 @@ function csseta()
     create_tags
     create_dict
 
-    echo "create  success"
+    echo_msg "create  success"
 }
 
 function cstag()
 {
     if [ -e "cscope.files" ]; then
-        echo "create cscope"
+        echo_msg "create cscope"
         cscope -bki cscope.files
-        echo "create tags"
+        echo_msg "create tags"
         ctags --c-kinds=-m --c++-kinds=-m --fields=+iaS --extra=+q  -L cscope.files
         sed 's/\s.*$//g' tags | uniq >  dict
     else
-        echo "no cscope.files"
+        echo_msg "no cscope.files"
     fi
 }
 
@@ -95,7 +118,7 @@ function delete_cscope_tags()
        rm  -rf cscope* tags TAGS ncscope* dict
        cd $cur_dir
    else
-       echo "$@ is not directory!!"
+       echo_msg "$@ is not directory!!"
    fi
 }
 
