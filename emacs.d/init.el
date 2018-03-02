04a4f404e504bbd8bfa01fcee4ebd666f23558c7
Jingyu Li <jingyuli@rdamicro.com>
2018-03-02 15:28:43 +0800

    for helm in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -58,3 +58,9 @@
 	     (define-key gtags-mode-map "\M-/" 'gtags-find-pattern)
 	     (define-key gtags-mode-map "\M-f" 'gtags-find-file)
 	     ))
+
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
