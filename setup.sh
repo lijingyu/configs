33e4e2bb55dfcd3e597f00ff6ca7e530e93ec0bc
jingyu.li <jingyu.li@cloudminds.com>
2018-07-09 09:43:21 +0800

    add file type .hal to cscope.files

--- setup.sh
+++ setup.sh
@@ -43,13 +43,13 @@ function create_cscopefiles()
             "Linux")
                 find  ${arg}  -type f -regextype posix-egrep \
                     -iregex '.*\/(makefile|Kconfig)' -prune -o \
-                    -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec)'\
+                    -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec|hal)'\
                     |sed  -e '/ /d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
 
             "Darwin")
                 find -E ${arg}  -type f \
                     -iregex '.*\/(makefile|Kconfig)' -prune -o \
-                    -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec)'\
+                    -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec|hal)'\
                     |sed  -e '/ /d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
         esac
     done
