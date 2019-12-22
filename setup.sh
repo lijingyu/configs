09622a14421936eb705b3e5b806fdcb95395e082
jingyu.li <jingyu.li@cloudminds.com>
2019-12-22 14:00:43 +0800

    remove .xxx in cscope.file

--- setup.sh
+++ setup.sh
@@ -61,13 +61,13 @@ function create_cscopefiles()
                 find  ${arg}  -type f -regextype posix-egrep \
                     -iregex '.*\/(makefile|Kconfig)' -prune -o \
                     -type f -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec|hal|hpp|cxx|hh|hxx|h++|cu)'\
-                    |sed  -e '/ /d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
+                    |sed  -e '/ /d' -e '/\/\./d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
 
             "Darwin")
                 find -E ${arg}  -type f \
                     -iregex '.*\/(makefile|Kconfig)' -prune -o \
                     -type f -regex '.*\.(c|h|m|s|S|java|sh|cpp|vim|hp|aidl|rc|py|cc|def|xml|mk|el|lisp|dtsi|dts|ss|y|lex|gperf|inf|dec|hal|hpp|cxx|hh|hxx|h++|cu)'\
-                    |sed  -e '/ /d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
+                    |sed  -e '/ /d' -e '/\/\./d' -e 's:^\./::' |grep -v "$EXINCLUDE_DIRS" >> cscope.files;;
         esac
     done
 }
